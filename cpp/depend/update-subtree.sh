a048281c7bed14f12bb2b54745e7a2ca85f162d4
Elichai Turkel <elichai.turkel@gmail.com>
2020-06-02 17:12:11 +0300

    CPP: fix update-subtree

--- cpp/depend/update-subtree.sh
+++ cpp/depend/update-subtree.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# This command pulls the latest updates from the master branch of the subtree and commits them in depend/secp256k1.
+# This command pulls the latest updates from the master branch of the subtrees.
 # See man git-subtree(1) for more details.
 git subtree pull --prefix cpp/depend/benchmark https://github.com/google/benchmark.git master --squash
+git subtree pull --prefix cpp/depend/benchmark/googletest https://github.com/google/googletest.git master --squash
